7e49c3af544432b4c4548827a0296cc96a99be1f
Edgar Zhang <edgarzzc@umich.edu>
2024-09-30 11:26:17 -0400

    zzc

--- insta485/__init__.py
+++ insta485/__init__.py
@@ -1,8 +1,11 @@
 """Insta485 package initializer."""
 import flask
 
+
+
 # app is a single object used by all the code modules in this package
 app = flask.Flask(__name__)  # pylint: disable=invalid-name
+
 
 # Read settings from config module (insta485/config.py)
 app.config.from_object('insta485.config')
