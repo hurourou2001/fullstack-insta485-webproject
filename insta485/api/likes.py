8a699068c813c8a75506b1fc6a7d3998461f22f6
chenruih <chenruih@umich.edu>
2024-10-02 22:44:14 -0400

    completed likes and fix small error on posts

---
+++ insta485/api/likes.py
@@ -0,0 +1,81 @@
+import flask
+import insta485
+from insta485.views.accounts import hash_password
+
+@insta485.app.route('/api/v1/likes/', methods=["POST"])
+def create_like():
+    connection = insta485.model.get_db()
+    if not insta485.api.posts.is_authenticated(connection):
+        return flask.jsonify({"message": "Unauthorized", "status_code": 403}),403
+    
+    postid = flask.request.args.get('postid', type=int)
+    url = flask.request.path
+    username = ""
+    if "username" in flask.session:
+        username = flask.session["username"]
+    else:
+        username = flask.request.authorization.username
+    
+    if postid is None:
+        return flask.jsonify({"message": "no postid", "status_code": 400}), 400
+    
+    check_exist_like = """
+        SELECT likeid
+        FROM likes
+        WHERE postid = ? AND owner = ?
+    """
+    cur = connection.execute(check_exist_like, (postid, username))
+    rst = cur.fetchone()
+    if rst is not None:
+        return flask.jsonify({"likeid": rst["likeid"],
+                              "url": f"{url}{rst['likeid']}/"}), 200
+    
+    add_like = """
+        INSERT INTO likes (owner, postid)
+        VALUES (?, ?)
+    """
+    cur = connection.execute(add_like, (username, postid))
+    likeid = cur.lastrowid
+
+    return flask.jsonify({
+        "likeid": likeid,
+        "url": f"{url}{likeid}/"
+    }), 201
+
+
+@insta485.app.route('/api/v1/likes/<int:likeid>/', methods=["DELETE"])
+def delete_like(likeid):
+    connection = insta485.model.get_db()
+    if not insta485.api.posts.is_authenticated(connection):
+        return flask.jsonify({"message": "Unauthorized", "status_code": 403}),403
+    
+    if "username" in flask.session:
+        user = flask.session["username"]
+    else:
+        user = flask.request.authorization.get("username")
+    check_q = """
+        SELECT likeid, owner
+        FROM likes
+        WHERE likeid = ?
+    """
+
+    rst = connection.execute(check_q, (likeid,)).fetchone()
+    if rst is None:
+        return flask.jsonify({
+                            "message": "Like not found",
+                            "status_code": 404
+                            }), 404
+
+    if rst["owner"] != user:
+        return flask.jsonify({
+                            "message": "Forbidden",
+                            "status_code": 403
+                            }), 403
+    
+    delete_q = """
+        DELETE FROM likes
+        WHERE likeid = ?
+    """
+    connection.execute(delete_q, (likeid,))
+    return "", 204
+
--- insta485/api/posts.py
+++ insta485/api/posts.py
@@ -36,9 +36,12 @@ def is_authenticated(connection):
 
     # 2. Session-based Authentication
     if 'username' in flask.session:
-        return True
-
-    return False
+        username = flask.session['username']
+        password = flask.session.get('password')
+        if password is None:
+            return False
+        
+    return verify_auth(connection, username, password)
 
     
 
@@ -88,7 +91,7 @@ def get_posts():
                                  (username, username)).fetchone()
         postid_lte = rst["postid"]
         
-    if size == 0 or page < 0:
+    if size <= 0 or page < 0:
         return flask.jsonify({"message": "Bad Request",
                               "status_code": 400}), 400
     url = flask.request.full_path
@@ -138,198 +141,14 @@ def get_post_id(postid_url_slug):
       "url": "/api/v1/posts/1/"
     }
     """
-    connection = insta485.model.get_db()
-
-    if not is_authenticated(connection):
-        return flask.jsonify({"message": "Unauthorized", "status_code": 403}),403
-    
-    if 'username' in flask.session:
-        username = flask.session['username']
-    else:
-        auth = flask.request.authorization
-        username = auth.username
-    
-    postid = postid_url_slug
-
-    queryPosts = connection.execute("""
-        SELECT created, filename, owner                        
-        FROM posts
-        WHERE postid = ?
-        """
-    ,(postid,)).fetchone()
-
-    if queryPosts is None:
-        return flask.jsonify({"message":"Not    Found","Status_code":404}),404
-
-    queryComments = connection.execute("""
-          SELECT commentid, owner, text, created
-          FROM comments
-          WHERE postid = ?
-        """
-        , (postid,)).fetchall()
-    
-    queryLike = connection.execute("""
-        SELECT likeid
-        FROM likes
-        WHERE postid = ? AND owner = ?
-        """, (postid, username)).fetchone()
-    
-    queryLikeNum = connection.execute("""
-        SELECT likeid
-        FROM likes
-        WHERE postid = ?
-        """, (postid,)).fetchall()
-    
-    if queryLike is not None:
-        Likes = {
-            "lognameLikesThis": True,
-            "numLikes": len(queryLikeNum),
-            "url":f"/api/v1/likes/{queryLike['likeid']}/"
-        }
-    else:
-        Likes = {
-            "lognameLikesThis": False,
-             "numLikes": len(queryLikeNum),
-            "url":"null"
-        }
-    
-    queryUser = connection.execute(
-        """
-        SELECT filename
-        FROM users
-        WHERE username = ?
-        """,(queryPosts['owner'],)).fetchone()
-    
-    response = {
-        "comments" :[
-            {
-                "commentid": comment['commentid'],
-                "lognameOwnsThis":True if username == comment['owner'] else False,
-                "owner":comment['owner'],
-                "ownerShowUrl":f"/users/{comment['owner']}/",
-                "text":comment['text'],
-            "url":f"/api/v1/comments/{comment['commentid']}/"
-            }
-            for comment in queryComments 
-        ],
-        "comments_url": f"/api/v1/comments/?postid={postid}",
-        "created": queryPosts['created'],
-        "imgUrl": f"/uploads/{queryPosts['filename']}",
-        "likes" : Likes,
-        "owner": queryPosts['owner'],
-        "ownerImgUrl": f"/uploads/{queryUser['filename']}",
-        "ownerShowUrl": f"/users/{queryPosts['owner']}/",
-        "postShowUrl": f"/posts/{postid}/",
-        "postid":postid,
-        "url": f"/api/v1/posts/{postid}/"
+    context = {
+        "created": "2017-09-28 04:33:28",
+        "imgUrl": "/uploads/122a7d27ca1d7420a1072f695d9290fad4501a41.jpg",
+        "owner": "awdeorio",
+        "ownerImgUrl": "/uploads/e1a7c5c32973862ee15173b0259e3efdb6a391af.jpg",
+        "ownerShowUrl": "/users/awdeorio/",
+        "postShowUrl": f"/posts/{postid_url_slug}/",
+        "postid": postid_url_slug,
+        "url": flask.request.path,
     }
-    
-    return flask.jsonify(response),200
-
-@insta485.app.route("/api/v1/comments/", methods=["POST"])
-def addComment():
-    """
-    Update a post with a comment.
-    """
-    connection = insta485.model.get_db()
-
-    if not is_authenticated(connection):
-        return flask.jsonify({"message": "Unauthorized", "status_code": 403}),403
-    
-    if 'username' in flask.session:
-        username = flask.session['username']
-    else:
-        auth = flask.request.authorization
-        username = auth.username
-
-    text = flask.request.json.get("text")
-    postid = flask.request.args.get('postid')
-    queryPosts = connection.execute("""
-        SELECT * FROM posts WHERE postid = ?""",    
-        (postid,)).fetchone()
-    
-    if not queryPosts:
-        return flask.jsonify({"message":"Not Found", "Status Code": 404}),404                      
-                                    
-    connection.execute(
-        """
-        INSERT INTO comments(postid, owner, text)
-        VALUES(?,?,?)
-        """, (postid, username, text))
-    
-    newComment = connection.execute(
-        """
-        SELECT last_insert_rowid() AS commentid
-        """
-    ).fetchone()['commentid']
-
-    
-
-    response = {
-         "commentid": newComment,
-        "lognameOwnsThis": True,
-         "owner": username,
-        "ownerShowUrl": f"/users/{username}/",
-        "text": text,
-        "url": f"/api/v1/comments/{newComment}/"
-    }
-
-    return flask.jsonify(response),201
-
-@insta485.app.route("/api/v1/comments/<commentid>/", methods = ["DELETE"])
-def deleteComment(commentid):
-    connection = insta485.model.get_db()
-
-    if not is_authenticated(connection):
-        return flask.jsonify({"message": "Unauthorized", "status_code": 
-                              403}),403
-    
-    if 'username' in flask.session:
-        username = flask.session['username']
-    else:
-        auth = flask.request.authorization
-        username = auth.username
-    
-    if not commentid:
-        return flask.jsonify({"message":"Not Found", "Status Code": 
-                              404}),404
-    
-    comment = connection.execute("""
-        SELECT * FROM comments WHERE commentid = ?""",
-        (commentid,)).fetchone()
-    
-    if not comment:
-        return flask.jsonify({"message":"Not Found", "Status Code": 404}),404
-   
-    commentOwner = connection.execute("""
-    SELECT owner FROM comments WHERE commentid = ?""",
-    (commentid,)).fetchone()['owner']
-    
-    if username != commentOwner:
-        return flask.jsonify({"message": "Unauthorized", "status_code": 
-                              403}),403
-
-    connection.execute(
-        """
-        DELETE FROM comments WHERE commentid = ?
-        """, (commentid,))
-    
-    connection.commit()
-
-    return '', 204
-
-    
-
-
-
-
-    
-
-
-                        
-
-
-    
-       
-    
-
+    return flask.jsonify(**context)
